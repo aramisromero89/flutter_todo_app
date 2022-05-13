d9d6f59f9dbea877a28051cf195c25abc55aab57
Aramis Romero Carballea <aromero@pyxelsolution.com>
2022-05-13 10:17:11 -0400

    ref: improve mock task repository

--- test/mock/mock_task_repository.dart
+++ test/mock/mock_task_repository.dart
@@ -2,23 +2,37 @@ import 'package:flutter_todo_app/model/entity/task.dart';
 import 'package:flutter_todo_app/model/repository/task_repository.dart';
 
 class MockTaskRepository extends TaskRepository {
+  int _currentIndex = 0;
+  final List<Task> _items = [];
   @override
   Future<List<Task>> create(String userId, String text) async {
-    return [Task(id: "a", text: text)];
+    _items.add(Task(id: (_currentIndex++).toString(), text: text));
+    return _items;
   }
 
   @override
   Future<Task?> delete(String taskId) async {
-    return Task(id: taskId, text: "a");
+    final index = _items.indexWhere((task) => task.id == taskId);
+    if (index < 0) {
+      return null;
+    }
+    final item = _items[index];
+    _items.removeAt(index);
+    return item;
   }
 
   @override
   Future<Task?> edit(String taskId, String text) async {
-    return Task(id: taskId, text: text);
+    final index = _items.indexWhere((task) => task.id == taskId);
+    if (index < 0) {
+      return null;
+    }
+    _items.replaceRange(index, 1, [Task(id: taskId, text: text)]);
+    return _items[index];
   }
 
   @override
   Future<List<Task>> list() async {
-    return [];
+    return _items;
   }
 }
