214ffd8ea16d8930bb590961ee0c3d40984cc2a1
Aramis Romero Carballea <aromero@pyxelsolution.com>
2022-05-14 07:37:57 -0400

    feat: add signup provider

---
+++ lib/provider/signup_provider.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'auth_provider.dart';
+
+class SignUpProvider extends ChangeNotifier {
+  final _authProvider = GetIt.I<AuthProvider>();
+  final formKey = GlobalKey<FormState>();
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final passwordConfirmController = TextEditingController();
+  bool _submitting = false;
+  bool get submitting => _submitting;
+  bool _hidePassword = true;
+  bool get hidePassword => _hidePassword;
+  bool _hideConfirmPassword = true;
+  bool get hideConfirmPassword => _hideConfirmPassword;
+
+  void togglePasswordVisibility() {
+    _hidePassword = !_hidePassword;
+    notifyListeners();
+  }
+
+  void toggleConfirmPasswordVisibility() {
+    _hideConfirmPassword = !_hideConfirmPassword;
+    notifyListeners();
+  }
+
+  void cancel() {
+    _authProvider.signingUp = false;
+  }
+
+  Future<void> submit() async {
+    if (formKey.currentState!.validate()) {
+      _submitting = true;
+      notifyListeners();
+      try {
+        await _authProvider.signUp(usernameController.text, passwordController.text);
+        cancel();
+      } catch (e) {
+        _submitting = false;
+        notifyListeners();
+        rethrow;
+      }
+    }
+    _submitting = false;
+    notifyListeners();
+  }
+}
