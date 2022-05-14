290b79b49173d5b94d7591049fc7240fb20e1e5e
Aramis Romero Carballea <aromero@pyxelsolution.com>
2022-05-14 07:38:21 -0400

    feat: add signin provider

---
+++ lib/provider/signin_provider.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'auth_provider.dart';
+
+class SignInProvider extends ChangeNotifier {
+  final _authProvider = GetIt.I<AuthProvider>();
+  final formKey = GlobalKey<FormState>();
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool _submitting = false;
+  bool get submitting => _submitting;
+  bool _hidePassword = true;
+  bool get hidePassword => _hidePassword;
+
+  void togglePasswordVisibility() {
+    _hidePassword = !_hidePassword;
+    notifyListeners();
+  }
+
+  void signUp() {
+    _authProvider.signingUp = true;
+  }
+
+  Future<void> submit() async {
+    if (formKey.currentState!.validate()) {
+      _submitting = true;
+      notifyListeners();
+      try {
+        await _authProvider.signIn(usernameController.text, passwordController.text);
+      } catch (e) {
+        _submitting = false;
+        notifyListeners();
+        rethrow;
+      }
+    }
+    _submitting = false;
+    notifyListeners();
+  }
+}
