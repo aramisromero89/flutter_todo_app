f5a619d762b8d2932394904c76d7b6cf4a04287c
Aramis Romero Carballea <aromero@pyxelsolution.com>
2022-05-17 10:15:51 -0400

    feat: set language selection on bottom of drawer

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_todo_app/i18n/strings.g.dart';
 import 'package:flutter_todo_app/provider/auth_provider.dart';
 import 'package:flutter_todo_app/view/components/signin_view.dart';
 import 'package:flutter_todo_app/view/components/task_list_view.dart';
+import 'package:form_validator/form_validator.dart';
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 
@@ -31,50 +32,53 @@ class HomePage extends StatelessWidget {
 
   Drawer drawer(BuildContext context) {
     return Drawer(
-      child: ListView(
-        // Important: Remove any padding from the ListView.
-        padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-            decoration: const BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/todo.png'), fit: BoxFit.cover),
-              color: Colors.blue,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                image: DecorationImage(image: AssetImage('assets/todo.png'), fit: BoxFit.cover),
+                color: Colors.blue,
+              ),
+              child: Container(),
             ),
-            child: Container(),
-          ),
-          ListTile(
-            title: Row(
+            Row(
               children: [const Icon(Icons.account_box), Text(GetIt.I<AuthProvider>().session!.userName)],
             ),
-          ),
-          ListTile(title: Text(t.general.language)),
-          ...LocaleSettings.supportedLocales.map(
-            (locale) {
-              return ListTile(
-                  title: Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Text(locale.languageCode,
-                        style: TextStyle(
-                            color: LocaleSettings.currentLocale.flutterLocale.languageCode == locale.languageCode
-                                ? Theme.of(context).primaryColor
-                                : Colors.black)),
-                  ),
-                  onTap: () {
-                    LocaleSettings.setLocaleRaw(locale.languageCode);
-                    Navigator.popUntil(context, ModalRoute.withName('/'));
-                  });
-            },
-          ).toList(),
-          ListTile(
-            title: Row(
-              children: [const Icon(Icons.logout), Text(t.auth.signout)],
-            ),
-            onTap: () {
-              GetIt.I<AuthProvider>().signOut();
-              Navigator.popUntil(context, ModalRoute.withName('/'));
-            },
-          ),
-        ],
+            const Padding(padding: EdgeInsets.all(15)),
+            InkWell(
+                child: Row(
+                  children: [const Icon(Icons.logout), Text(t.auth.signout)],
+                ),
+                onTap: () {
+                  GetIt.I<AuthProvider>().signOut();
+                  Navigator.popUntil(context, ModalRoute.withName('/'));
+                }),
+            Expanded(
+                child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(children: [
+                ...LocaleSettings.supportedLocales.map(
+                  (locale) {
+                    return TextButton(
+                      onPressed: () {
+                        ValidationBuilder.setLocale(locale.languageCode);
+                        LocaleSettings.setLocaleRaw(locale.languageCode);
+                        Navigator.popUntil(context, ModalRoute.withName('/'));
+                      },
+                      child: Text(locale.languageCode,
+                          style: TextStyle(
+                              color: LocaleSettings.currentLocale.flutterLocale.languageCode == locale.languageCode
+                                  ? Theme.of(context).primaryColor
+                                  : Colors.black)),
+                    );
+                  },
+                ).toList(),
+              ]),
+            ))
+          ],
+        ),
       ),
     );
   }
