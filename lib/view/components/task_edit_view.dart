282d0373a1eba4c00101f35e264c6a304ab65ad9
Aramis Romero Carballea <aromero@pyxelsolution.com>
2022-05-17 17:14:06 -0400

    feat: remove developer dependency

--- lib/view/components/task_edit_view.dart
+++ lib/view/components/task_edit_view.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:flutter_todo_app/i18n/strings.g.dart';
